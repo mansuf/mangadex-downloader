268d269dd7ae35bb7c300f31097029403001d4a2
Rahman Yusuf <danipart4@gmail.com>
2022-01-21 22:48:43 +0700

    bump version 0.1.1

--- mangadex_downloader/__init__.py
+++ mangadex_downloader/__init__.py
@@ -2,7 +2,7 @@
 Download manga from Mangadex through Python
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __description__ = "Download manga from Mangadex through Python"
 __author__ = "mansuf"
 __license__ = "The Unlicense"
