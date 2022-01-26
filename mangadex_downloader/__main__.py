5df30ec02a2ba75e08c062b43915c9283b9a54f9
Rahman Yusuf <danipart4@gmail.com>
2022-01-26 13:41:43 +0700

    add typing for validating MangaDex URL

--- mangadex_downloader/__main__.py
+++ mangadex_downloader/__main__.py
@@ -3,7 +3,8 @@ import argparse
 import logging
 from .network import Net
 from .main import download
-from .utils import validate_url
+from .utils import validate_url as _validate
+from .errors import InvalidURL
 from . import __description__
 
 def setup_logging(name_module, verbose=False):
@@ -18,9 +19,16 @@ def setup_logging(name_module, verbose=False):
         log.setLevel(logging.INFO)
     return log
 
+def validate_url(url):
+    try:
+        _id = _validate(url)
+    except InvalidURL as e:
+        raise argparse.ArgumentTypeError(str(e))
+    return _id
+
 def _main(argv):
     parser = argparse.ArgumentParser(description=__description__)
-    parser.add_argument('URL', help='MangaDex URL')
+    parser.add_argument('URL', type=validate_url, help='MangaDex URL')
     parser.add_argument('--folder', metavar='FOLDER', help='Store manga in given folder')
     parser.add_argument('--proxy', metavar='SOCKS / HTTP Proxy', help='Set http/socks proxy')
     parser.add_argument('--proxy-env', action='store_true', help='use http/socks proxy from environments')
