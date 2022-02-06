8062c6b29365cb671fc227de14de08b4b34129b5
Rahman Yusuf <mansuf@mansuf.link>
2022-02-06 16:44:31 +0700

    remove one file bundled executable for security reasons

--- compile.py
+++ compile.py
@@ -1,16 +1,6 @@
 import sys
 from subprocess import run
 
-output_one_file = 'mangadex-dl-%s' % sys.platform
-
-run([
-    'pyinstaller',
-    'run.py',
-    '-F',
-    '-n',
-    output_one_file
-])
-
 output_one_folder = 'mangadex-dl'
 
 run([
