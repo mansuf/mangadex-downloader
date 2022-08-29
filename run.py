eb47c6b03ef2556926858b06bafaa34652c52d45
Rahman Yusuf <mansuf@mansuf.link>
2022-08-29 09:11:07 +0700

    explain why `run.py` was created

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+# This was used to run mangadex-downloader for compiled app
+# Because we cannot compile __main__.py directly (i don't know why, the errors are confusing)
+
 from mangadex_downloader.__main__ import main
 
 if __name__ == "__main__":
