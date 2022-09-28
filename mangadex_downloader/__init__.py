e99f3a9ff10f6ef5b4dd72610cee31deb9229c36
mansuf <mansuf@mansuf.link>
2022-09-28 13:24:40 +0700

    bump version 2.1.0a
    starting the development of new features

--- mangadex_downloader/__init__.py
+++ mangadex_downloader/__init__.py
@@ -2,7 +2,7 @@
 A command-Line tool to download manga from MangaDex, written in Python
 """
 
-__version__ = "2.0.4"
+__version__ = "2.1.0a"
 __description__ = "A Command-line tool to download manga from MangaDex, written in Python"
 __author__ = "Rahman Yusuf"
 __author_email__ = "danipart4@gmail.com"
