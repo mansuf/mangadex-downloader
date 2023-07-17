74a96d77acdf067cbd7b46892079ab1c8b8a369d
Rahman Yusuf <mansuf@mansuf.link>
2023-07-17 21:50:34 +0700

    Fix setup is not working
    due to description regex is failing because of black formatter

--- mangadex_downloader/__init__.py
+++ mangadex_downloader/__init__.py
@@ -2,15 +2,15 @@
 A command-Line tool to download manga from MangaDex, written in Python
 """
 
+# fmt: off
 __version__ = "2.10.3"
-__description__ = (
-    "A Command-line tool to download manga from MangaDex, written in Python"
-)
+__description__ = "A Command-line tool to download manga from MangaDex, written in Python"
 __author__ = "Rahman Yusuf"
 __author_email__ = "danipart4@gmail.com"
 __license__ = "MIT"
 __repository__ = "mansuf/mangadex-downloader"
 __url_repository__ = "https://github.com"
+# fmt: on
 
 import logging
 
