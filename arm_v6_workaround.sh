c4df7596ca00dd40612677c26cfbc6a09751b03e
Rahman Yusuf <mansuf@mansuf.link>
2024-06-07 13:49:52 +0700

    Test architecture arm/v6 (part 9)

--- arm_v6_workaround.sh
+++ arm_v6_workaround.sh
@@ -1,13 +1,15 @@
 # Workaround to install rust for building Dockerfile.optional in ARM v6 machine CI
 echo $(lscpu)
 export ARCHITECTURE=$(lscpu | grep Architecture | sed -e 's/Architecture:\s//g')
+export CPU_FLAGS=$(lscpu | grep Flags | sed -e 's/Flags:\s//g')
 echo $ARCHITECTURE
-if [ $ARCHITECTURE = "armv7l" ]; then 
+echo $CPU_FLAGS
+
+echo $ARCHITECTURE
+if [ $ARCHITECTURE = "armv7l" ] && ! [[ $CPU_FLAGS =~ "v_vmsave_vmlo" ]];; then 
     apt install libc6-armhf-cross
     export LD_LIBRARY_PATH=/usr/arm-linux-gnueabihf/lib
     ln -s /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 /lib/ld-linux-armhf.so.3
-    # ln -s /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 /usr/lib/ld-linux-armhf.so.3
-    # export QEMU_LD_PREFIX=/usr/arm-linux-gnueabi
 else 
     raise_error_lol_command_not_found; 
 fi
