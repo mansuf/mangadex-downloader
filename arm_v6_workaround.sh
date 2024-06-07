13c9fba32cb6aabc8a9c354ab0e612694eccf664
Rahman Yusuf <mansuf@mansuf.link>
2024-06-07 14:06:33 +0700

    Test architecture arm/v6 (part 16)

--- arm_v6_workaround.sh
+++ arm_v6_workaround.sh
@@ -11,7 +11,7 @@ echo $CPU_FLAGS
 apt update
 apt install libc6-armhf-cross
 # export LD_LIBRARY_PATH=/usr/arm-linux-gnueabihf/lib
-export QEMU_LD_PREFIX=/usr/arm-linux-gnueabi
+# export QEMU_LD_PREFIX=/usr/arm-linux-gnueabi
 ln -s /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 /lib/ld-linux-armhf.so.3
 # if [ $ARCHITECTURE = "armv7l" ] && ! [[ $CPU_FLAGS =~ "v_vmsave_vmlo" ]]; then 
 #     apt install libc6-armhf-cross
