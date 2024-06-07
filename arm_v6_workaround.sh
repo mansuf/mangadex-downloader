fa442c96ade0372d8da9b84959f05de03727c19d
Rahman Yusuf <mansuf@mansuf.link>
2024-06-07 13:29:47 +0700

    Test architecture arm/v6 (part 8)

--- arm_v6_workaround.sh
+++ arm_v6_workaround.sh
@@ -4,7 +4,9 @@ export ARCHITECTURE=$(lscpu | grep Architecture | sed -e 's/Architecture:\s//g')
 echo $ARCHITECTURE
 if [ $ARCHITECTURE = "armv7l" ]; then 
     apt install libc6-armhf-cross
-    ln -s /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 /usr/lib/ld-linux-armhf.so.3
+    export LD_LIBRARY_PATH=/usr/arm-linux-gnueabihf/lib
+    ln -s /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 /lib/ld-linux-armhf.so.3
+    # ln -s /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 /usr/lib/ld-linux-armhf.so.3
     # export QEMU_LD_PREFIX=/usr/arm-linux-gnueabi
 else 
     raise_error_lol_command_not_found; 
