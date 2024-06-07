8c27dcebded1c2557ae96ab83a99264551d5a7b2
Rahman Yusuf <mansuf@mansuf.link>
2024-06-07 13:18:09 +0700

    Test architecture arm/v6 (part 6)
    it's an L lower not 1, lmao

--- arm_v6_workaround.sh
+++ arm_v6_workaround.sh
@@ -1,7 +1,7 @@
 # Workaround to install rust for building Dockerfile.optional in ARM v6 machine CI
 export ARCHITECTURE=$(lscpu | grep Architecture | sed -e 's/Architecture:\s//g')
 echo $ARCHITECTURE
-if [ $ARCHITECTURE = "armv71" ]; then 
+if [ $ARCHITECTURE = "armv7l" ]; then 
     export QEMU_LD_PREFIX=/usr/arm-linux-gnueabi
 else 
     raise_error_lol_command_not_found; 
