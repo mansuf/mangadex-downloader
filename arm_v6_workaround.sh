c5bffebab042e0437a5a93d98d80865c2ec8e4df
Rahman Yusuf <mansuf@mansuf.link>
2024-06-07 13:13:15 +0700

    Test architecture arm/v6 (part 4)

---
+++ arm_v6_workaround.sh
@@ -0,0 +1,8 @@
+# Workaround to install rust for building Dockerfile.optional in ARM v6 machine CI
+export ARCHITECTURE=$(lscpu | grep Architecture | sed -e 's/Architecture:\s//g')
+echo $ARCHITECTURE
+if [ "$ARCHITECTURE" = "armv71" ]; then 
+    export QEMU_LD_PREFIX=/usr/arm-linux-gnueabi
+else 
+    raise_error_lol_command_not_found; 
+fi
